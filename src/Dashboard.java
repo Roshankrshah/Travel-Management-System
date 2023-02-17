1d7058f61690c34c47446ffcebedccd41ea3f973
Roshan <shahroshan0720@gmail.com>
2023-02-17 17:07:03 +0530

    Dashboard ui completed

---
+++ src/Dashboard.java
@@ -0,0 +1,179 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Dashboard extends JFrame {
+
+    Dashboard(){
+        //setBounds(0,0,1600,1000);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setLayout(null);
+
+        JPanel p1 = new JPanel();
+        p1.setLayout(null);
+        p1.setBounds(0,0,1600,65);
+        p1.setBackground(new Color(0,0,102));
+        add(p1);
+
+        ImageIcon i1 = new ImageIcon("D:\\Travel Management System\\icons\\dashboard.png");
+        Image i2 = i1.getImage().getScaledInstance(70,70,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel icon = new JLabel(i3);
+        icon.setBounds(5,0,70,70);
+        p1.add(icon);
+
+        JLabel heading  = new JLabel("Dashboard");
+        heading.setBounds(80,10,300,40);
+        heading.setForeground(Color.WHITE);
+        heading.setFont(new Font("Tahoma",Font.BOLD,30));
+        p1.add(heading);
+
+
+        JPanel p2 = new JPanel();
+        p2.setLayout(null);
+        p2.setBounds(0,65,300,900);
+        p2.setBackground(new Color(0,0,102));
+        add(p2);
+
+        JButton addPersonalDetails = new JButton("Add Personal Details");
+        addPersonalDetails.setBounds(0,0,300,50);
+        addPersonalDetails.setBackground(new Color(0,0,120));
+        addPersonalDetails.setForeground(Color.WHITE);
+        addPersonalDetails.setFont(new Font("Tahoma",Font.PLAIN,20));
+        addPersonalDetails.setMargin(new Insets(0,0,0,40));
+        p2.add(addPersonalDetails);
+
+        JButton updatePersonalDetails = new JButton("Update Personal Details");
+        updatePersonalDetails.setBounds(0,50,300,50);
+        updatePersonalDetails.setBackground(new Color(0,0,120));
+        updatePersonalDetails.setForeground(Color.WHITE);
+        updatePersonalDetails.setFont(new Font("Tahoma",Font.PLAIN,20));
+        updatePersonalDetails.setMargin(new Insets(0,0,0,20));
+        p2.add(updatePersonalDetails);
+
+        JButton viewPersonalDetails = new JButton("View Personal Details");
+        viewPersonalDetails.setBounds(0,100,300,50);
+        viewPersonalDetails.setBackground(new Color(0,0,120));
+        viewPersonalDetails.setForeground(Color.WHITE);
+        viewPersonalDetails.setFont(new Font("Tahoma",Font.PLAIN,20));
+        viewPersonalDetails.setMargin(new Insets(0,0,0,30));
+        p2.add(viewPersonalDetails);
+
+        JButton deletePersonalDetails = new JButton("Delete Personal Details");
+        deletePersonalDetails.setBounds(0,150,300,50);
+        deletePersonalDetails.setBackground(new Color(0,0,120));
+        deletePersonalDetails.setForeground(Color.WHITE);
+        deletePersonalDetails.setFont(new Font("Tahoma",Font.PLAIN,20));
+        deletePersonalDetails.setMargin(new Insets(0,0,0,20));
+        p2.add(deletePersonalDetails);
+
+        JButton checkpackages = new JButton("Check Packages");
+        checkpackages.setBounds(0,200,300,50);
+        checkpackages.setBackground(new Color(0,0,120));
+        checkpackages.setForeground(Color.WHITE);
+        checkpackages.setFont(new Font("Tahoma",Font.PLAIN,20));
+        checkpackages.setMargin(new Insets(0,0,0,80));
+        p2.add(checkpackages);
+
+        JButton bookpackage = new JButton("Book Packages");
+        bookpackage.setBounds(0,250,300,50);
+        bookpackage.setBackground(new Color(0,0,120));
+        bookpackage.setForeground(Color.WHITE);
+        bookpackage.setFont(new Font("Tahoma",Font.PLAIN,20));
+        bookpackage.setMargin(new Insets(0,0,0,90));
+        p2.add(bookpackage);
+
+        JButton viewpackage = new JButton("View Packages");
+        viewpackage.setBounds(0,300,300,50);
+        viewpackage.setBackground(new Color(0,0,120));
+        viewpackage.setForeground(Color.WHITE);
+        viewpackage.setFont(new Font("Tahoma",Font.PLAIN,20));
+        viewpackage.setMargin(new Insets(0,0,0,90));
+        p2.add(viewpackage);
+
+        JButton viewhotels = new JButton("View Hotels");
+        viewhotels.setBounds(0,350,300,50);
+        viewhotels.setBackground(new Color(0,0,120));
+        viewhotels.setForeground(Color.WHITE);
+        viewhotels.setFont(new Font("Tahoma",Font.PLAIN,20));
+        viewhotels.setMargin(new Insets(0,0,0,110));
+        p2.add(viewhotels);
+
+        JButton bookhotel = new JButton("Book Hotel");
+        bookhotel.setBounds(0,400,300,50);
+        bookhotel.setBackground(new Color(0,0,120));
+        bookhotel.setForeground(Color.WHITE);
+        bookhotel.setFont(new Font("Tahoma",Font.PLAIN,20));
+        bookhotel.setMargin(new Insets(0,0,0,120));
+        p2.add(bookhotel);
+
+        JButton viewbookhotel = new JButton("View Book Hotel");
+        viewbookhotel.setBounds(0,450,300,50);
+        viewbookhotel.setBackground(new Color(0,0,120));
+        viewbookhotel.setForeground(Color.WHITE);
+        viewbookhotel.setFont(new Font("Tahoma",Font.PLAIN,20));
+        viewbookhotel.setMargin(new Insets(0,0,0,70));
+        p2.add(viewbookhotel);
+
+        JButton destinations = new JButton("Destinations");
+        destinations.setBounds(0,500,300,50);
+        destinations.setBackground(new Color(0,0,120));
+        destinations.setForeground(Color.WHITE);
+        destinations.setFont(new Font("Tahoma",Font.PLAIN,20));
+        destinations.setMargin(new Insets(0,0,0,100));
+        p2.add(destinations);
+
+        JButton payment = new JButton("Payment");
+        payment.setBounds(0,550,300,50);
+        payment.setBackground(new Color(0,0,120));
+        payment.setForeground(Color.WHITE);
+        payment.setFont(new Font("Tahoma",Font.PLAIN,20));
+        payment.setMargin(new Insets(0,0,0,130));
+        p2.add(payment);
+
+        JButton calculator = new JButton("Calculator");
+        calculator.setBounds(0,600,300,50);
+        calculator.setBackground(new Color(0,0,120));
+        calculator.setForeground(Color.WHITE);
+        calculator.setFont(new Font("Tahoma",Font.PLAIN,20));
+        calculator.setMargin(new Insets(0,0,0,120));
+        p2.add(calculator);
+
+        JButton notepad = new JButton("Notepad");
+        notepad.setBounds(0,650,300,50);
+        notepad.setBackground(new Color(0,0,120));
+        notepad.setForeground(Color.WHITE);
+        notepad.setFont(new Font("Tahoma",Font.PLAIN,20));
+        notepad.setMargin(new Insets(0,0,0,130));
+        p2.add(notepad);
+
+
+
+        JButton about = new JButton("About");
+        about.setBounds(0,700,300,50);
+        about.setBackground(new Color(0,0,120));
+        about.setForeground(Color.WHITE);
+        about.setFont(new Font("Tahoma",Font.PLAIN,20));
+        about.setMargin(new Insets(0,0,0,150));
+        p2.add(about);
+
+
+        ImageIcon i4 = new ImageIcon("D:\\Travel Management System\\icons\\home.jpg");
+        Image i5 = i4.getImage().getScaledInstance(1650,1000,Image.SCALE_DEFAULT);
+        ImageIcon i6 = new ImageIcon(i5);
+        JLabel image = new JLabel(i6);
+        image.setBounds(0,0,1650,1000);
+        add(image);
+
+        JLabel text = new JLabel("Travel and Tourism Management System");
+        text.setBounds(400,70,1200,70);
+        text.setForeground(Color.WHITE);
+        text.setFont(new Font("Raleway",Font.PLAIN,55));
+        image.add(text);
+
+        setVisible(true);
+    }
+
+    public static void main(String []args){
+        new Dashboard();
+    }
+}
